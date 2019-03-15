9d555e5a3205942a095c29792155c0eea5198c8a
IlianaGenova <iliana.genova.02@gmail.com>
2019-03-15 18:22:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var height = Number(document.getElementById("height").value);
+var weight = Number(document.getElementById("weight").value);
+var age = Number(document.getElementById("age").value);
+var gender = document.getElementById("gender").value;
+
+
+var BMI = weight/(height/100)*(height/100);
+var cal;
+switch (gender) {
+  case "male":
+    cal = 10*weight+6.25*height-5*age+5;
+    break;
+  case "female":
+    cal = 10*weight+6.25*height-5*age-161;
+    break;
+}
+
+function print_bmi(){
+  document.getElementById("bmi").innerHTML = BMI;
+}
