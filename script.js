67f7b62695eacbc73f642fa4a44878d6e0d4a31d
Radevskii <37086021+Radevskii@users.noreply.github.com>
2019-03-16 09:38:18 +0200

    Calculate

--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-var height = Number(document.getElementById("height").value);
-var weight = Number(document.getElementById("weight").value);
-var age = Number(document.getElementById("age").value);
-var gender = document.getElementById("gender").value;
+function Calculate()
+{
+  var height = parseFloat(document.getElementById("height").value);
+  var weight = parseFloat(document.getElementById("weight").value);
+  var age = parseFloat(document.getElementById("age").value);
+  var gender = document.getElementById("gender");
+  var BMI = (weight)/((height/100)*(height/100));
+  var cal;
+  switch (gender) {
+    case "male":
+      cal = 10*weight+6.25*height-5*age+5;
+      break;
+    case "female":
+      cal = 10*weight+6.25*height-5*age-161;
+      break;
 
+  }
+  print_bmi(BMI);
+  change();
 
-var BMI = weight/(height/100)*(height/100);
-var cal;
-switch (gender) {
-  case "male":
-    cal = 10*weight+6.25*height-5*age+5;
-    break;
-  case "female":
-    cal = 10*weight+6.25*height-5*age-161;
-    break;
 }
-
-function print_bmi(){
+function print_bmi(BMI){
   document.getElementById("bmi").innerHTML = BMI;
 }
 
 function hide_second() {
-  document.getElementById("second").style.display = "none";
+  document.getElementById("second").style.display = "hide";
 }
 
 function change() {
