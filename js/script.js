e1973678649d38020ac984e73045f0f58bb4342e
IlianaGenova <iliana.genova.02@gmail.com>
2019-03-16 12:13:51 +0200

    calculations_update

--- js/script.js
+++ js/script.js
@@ -8,22 +8,27 @@ function Calculate()
   var height = parseFloat(document.getElementById("height").value);
   var weight = parseFloat(document.getElementById("weight").value);
   var age = parseFloat(document.getElementById("age").value);
-  var gender = document.getElementById("gender");
+  var gender = document.getElementById("gender").value;
   var BMI = (weight)/((height/100)*(height/100));
   var cal;
+  debugger
   switch (gender) {
-    case "male":
-      cal = 10*weight+6.25*height-5*age+5;
+    case "2":
+      cal = (13.75*weight)+(5*height)-(6.76*age)+66;
       break;
-    case "female":
-      cal = 10*weight+6.25*height-5*age-161;
+    case "1":
+      cal = (9.56*weight)+(1.85*height)-(4.68*age)+655;
       break;
 
   }
-  print_bmi(BMI);
+  print_bmi(BMI.toFixed(1));
+  print_cal(cal);
   change();
 
 }
 function print_bmi(BMI){
   document.getElementById("bmi").innerHTML = BMI;
+}
+function print_cal(cal){
+  document.getElementById("cal").innerHTML = cal;
 }
